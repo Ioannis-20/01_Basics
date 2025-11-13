6b30fb548c086eeee1ba860252ebe0ac62f4d88c
Ioannis <ioannis@example.com>
2025-11-13 13:12:59 +0100

    hhh

--- 41_If_else/assignment1.js
+++ 41_If_else/assignment1.js
@@ -1,5 +1,22 @@
 let number = 5;
 
 //check if number is greater than 0
-if (number > 0);
-    
+if (number > 0) {
+    console.log("The number is positive");
+ } else { 
+        console.log("The number is not positive");
+ }
+
+ //if the number is less than 0
+ if (number < 0)  {
+ console.log("The number is negative");
+ } else {
+    console.log("The number is positive or 0");
+ }  
+
+ //if the number equals 0
+ if(number == 0) {
+    console.log("The number is zero");
+  } else {
+    console.log("The number is not 0");
+  }
