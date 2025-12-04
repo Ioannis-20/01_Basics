84eccef0a55e1b18652629b1658d64a24207e4cb
Ioannis <ioannis@example.com>
2025-12-04 12:49:24 +0100

    45 Task2 Done

---
+++ 45_Deno/task2.js
@@ -0,0 +1,20 @@
+let sigma = 0
+
+
+if(sigma > 0){
+    console.log("number is positive");
+}else{
+    console.log("number is not positive");
+}
+
+if(sigma < 0){
+    console.log("the number is less than 0");
+}else{
+    console.log("the number is not less or 0");
+}
+
+if(sigma == 0){
+    console.log("the number is 0");
+}else{
+    console.log("the number is not 0");
+}
