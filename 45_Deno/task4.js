b5b600851a11acf9d0fa794588511ae6b0249d4b
Ioannis <ioannis@example.com>
2025-12-07 18:38:28 +0100

    task 4 done

---
+++ 45_Deno/task4.js
@@ -0,0 +1,19 @@
+let age = Number(prompt("Write down your age"));
+
+if (age < 18 ){
+    console.log("You are young");
+}else{
+    console.log("you are not young");
+}
+
+if (age >= 18 && age < 50){
+    console.log("you are middle aged");
+}else{
+    console.log("you are not middle aged");
+}
+
+if (age > 50){
+    console.log("you are old")
+}else{
+    console.log("you are not old")
+}
