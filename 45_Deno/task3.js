e01c66813424eec2cef2a87a99594af99d81a1fc
Ioannis <ioannis@example.com>
2025-12-18 12:37:58 +0100

    refactoring code 2

--- 45_Deno/task3.js
+++ 45_Deno/task3.js
@@ -3,27 +3,25 @@ let b = Number(prompt("Enter second number:"));
 let input = prompt("Enter operator (+, -, *, /):");
 
 if (input == "+") {
-    console.log("result is " + (a + b));
+  console.log("result is " + (a + b));
 } else {
-    console.log("operator is not plus");
+  console.log("operator is not plus");
 }
 
 if (input == "-") {
-    console.log("result is " + (a - b));
+  console.log("result is " + (a - b));
 } else {
-    console.log("operator is not minus");
+  console.log("operator is not minus");
 }
 
 if (input == "*") {
-    console.log("result is " + (a * b));
+  console.log("result is " + a * b);
 } else {
-    console.log("operator is not multiply");
+  console.log("operator is not multiply");
 }
 
 if (input == "/") {
-    console.log("result is " + (a / b));
+  console.log("result is " + a / b);
 } else {
-    console.log("operator is not divide");
+  console.log("operator is not divide");
 }
-
-
---
+++ 50_Logische_Operatoren/task1.js
@@ -0,0 +1,12 @@
+let Grade = Math.floor(Math.random() * 100);
+
+if (Grade >= 90){
+    console.log("Grade: A");
+} else if (score >= 80 && score <= 89) {
+    console.log("Grade: B");
+} else if (score >= 70 && score <= 79) {
+    console.log("Grade: C");
+} else if (score >= 60 && score <= 69) {
+    console.log("Grade: D");
+} else if {
+    console.log("Grade: F"
