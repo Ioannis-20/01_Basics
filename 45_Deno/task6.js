a415453c8892c9531730608644a3b9f61106f92f
Ioannis <ioannis@example.com>
2025-12-18 11:48:38 +0100

    45 task 6 done

---
+++ 45_Deno/task6.js
@@ -0,0 +1,15 @@
+let input = prompt("What would you like to Drink?");
+console.log("input" , input);
+
+if (input == "Wasser") {
+  console.log("Thank you for your order from Water");
+}
+if (input == "Beer") {
+  console.log("Thank you for your order from Beer");
+}
+if (input == "Cola") {
+  console.log("Thank you for your order from Cola");
+}
+if (input == "Tea") {
+  console.log("Thank you for your order from Tea");
+}
