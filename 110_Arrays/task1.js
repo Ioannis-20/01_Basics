527949a4db3978b2a26bc92e403c447773d1e3d3
Ioannis <ioannis@example.com>
2026-01-15 12:57:13 +0100

    arrays task 1 complete

---
+++ 110_Arrays/task1.js
@@ -0,0 +1,12 @@
+let arr = [];
+for(let i = 0;i < 7; i++) {
+    arr.push(Math.floor(Math.random()*200))
+}
+let summe = 0;
+for (let i = 0; i < arr.length; i++) {
+    summe = summe + arr[i];
+}
+
+
+console.log("Zahlen:", arr);
+console.log("Summe:", summe);
